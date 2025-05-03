183a71eb68029505639ac8d2b9212070bced096e
Ravindra Yadav <ravindrayadav95473@gmail.com>
2025-05-03 10:02:48 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,6 @@ dotenv.config()
  export const FormDB =()=>{
   mongoose.connect(process.env.MONGO_URL,
       useNewUrlParser:true, 
-      useUnifiedTopology:true,
       tlsInsecure:true
    ).then(()=>{
   console.log('Database Connected')
